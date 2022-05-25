8b75826b58e63d06ec4413de6854f4d699cb150c
Kofi Boateng <kdboat2@gmail.com>
2022-05-24 21:02:02 -0500

    Completed frontend to back end linkage. Will address devices during each roll out.

--- src/controllers/Profile/profileController.js
+++ src/controllers/Profile/profileController.js
@@ -6,39 +6,27 @@ import { _getUserInfo } from "../../utils/redisCache.js";
 const userInfo = async (req, res) => {
   const TOKEN = await req.get("authorization");
   const ORIGIN = await req.get("Origin");
-  const IP = req.socket.remoteAddress;
-
-  if (typeof TOKEN !== "string" || !TOKEN) {
+  const IP = (await req.get("x-forwarded-for")) || req.socket.remoteAddress;
+  try {
+    const CHECK = await _verify(TOKEN);
+    const USER = await _getUserInfo(CHECK.user);
+    res.status(200).json({
+      fName: USER.info.firstName,
+      lName: USER.info.lastName,
+      email: USER.info.email,
+      country: USER.info.country,
+      state: USER.info.state,
+      accountNum: USER.info.accountNumber,
+      routingNum: USER.info.routingNumber,
+      funds: parseFloat(USER.info.capital.toFixed(2)),
+      transactions: USER.info.transactions,
+      zipCode: USER.info.zipcode,
+      notis: USER.noti ? USER.noti : undefined,
+    });
+  } catch (error) {
+    console.log(error.message);
     res.status(401).json();
-    return;
   }
-
-  const CHECK = await _verify(TOKEN);
-  if (!CHECK) {
-    console.log(CHECK);
-    res.status(401).json("invalid");
-    return;
-  }
-
-  const USER = await _getUserInfo(CHECK.user);
-  if (!USER) {
-    res.status(401).json();
-    return;
-  }
-
-  res.status(200).json({
-    fName: USER.info.firstName,
-    lName: USER.info.lastName,
-    email: USER.info.email,
-    country: USER.info.country,
-    state: USER.info.state,
-    accountNum: USER.info.accountNumber,
-    routingNum: USER.info.routingNumber,
-    funds: parseFloat(USER.info.capital.toFixed(2)),
-    transactions: USER.info.transactions,
-    zipCode: USER.info.zipcode,
-    notis: USER.noti ? USER.noti : undefined,
-  });
 };
 
 export default userInfo;
--- src/controllers/authentication/refreshTokenController.js
+++ src/controllers/authentication/refreshTokenController.js
@@ -5,14 +5,15 @@ import { _sign, _verify } from "../../utils/jwtConfig.js";
 
 const getRefreshToken = async (req, res) => {
   const TOKEN = await req.query.token;
-  const EMAIL = await _verify(TOKEN);
-  const expiresIn = _config.EXPIRESIN;
-  if (!EMAIL) {
+  try {
+    const CHECK = await _verify(TOKEN);
+    const expiresIn = +_config.EXPIRESIN;
+    const NEWTOKEN = await _sign(CHECK.user);
+    res.status(200).json({ token: NEWTOKEN, expiresIn: expiresIn });
+  } catch (error) {
+    console.log(error.message);
     res.status(401).json();
-    return;
   }
-  const NEWTOKEN = await _sign(EMAIL);
-  res.status(200).json({ token: NEWTOKEN, expiresIn: expiresIn });
 };
 
 export default getRefreshToken;
